fd7c3d148d772b71aabbb7b02e5ee61d312789c2
shoham3 <shohampressler@hilma.tech>
2023-11-21 17:26:47 +0200

    starting to build a login page

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login () {
-    return ()
+function Login() {
+    const [username, setUsername] = useState('')
+  return (
+
+    function handleLogin() {
+
+    }
+
+    <main>
+      <form>
+        <label htmlFor="username">username</label>
+        <input id="username" type="text" />
+        <label htmlFor="password">password</label>
+        <input id="username" type="password" />
+        <button onClick={handleLogin} >Login</button>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
