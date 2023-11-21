f5d4e73ac28fdc8eb3ba636840fa803188916b8d
ori-eph <oriephrati@hilma.tech>
2023-11-21 17:30:14 +0200

    fix register in route

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
 import HomeLayout from "./components/HomeLayout";
 import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
